2f0542f00f12bc88c6310ce64d4fed73f1d3f175
winbo.yml <winbo.yml@proton.me>
2025-01-18 08:23:01 +0100

    xcover4lte: Add products list

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_xcover4lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_xcover4lte-user \
+    lineage_xcover4lte-userdebug \
+    lineage_xcover4lte-eng
